7299b6bffecece653ee95e7d75aa730309d544ba
simon555 <simon.sebbagh@free.fr>
2017-11-30 22:19:14 -0500

    new architecture`

--- AutoencoderModel.py
+++ AutoencoderModel.py
@@ -62,6 +62,39 @@ print("done in {} mini-batches of size {}".format(len(testloader),NbatchTest))
 N1=32
 N2=N1*N1
 
+class Block(nn.Module):
+    def __init__(self, Nchannels):        
+        super(Block, self).__init__()
+        self.Conv_1=nn.Conv2d(Nchannels,Nchannels,3,stride=1,padding=1) 
+        self.BN_1=torch.nn.BatchNorm2d(Nchannels,affine=False)
+        self.Conv_2=nn.Conv2d(Nchannels,Nchannels,3,stride=1,padding=1)
+        self.BN_2=torch.nn.BatchNorm2d(Nchannels,affine=False)
+        self.Conv_3=nn.Conv2d(Nchannels,Nchannels,3,stride=1,padding=1) 
+        self.BN_3=torch.nn.BatchNorm2d(Nchannels,affine=False)
+    
+    def forward(self,input):
+        x = self.Conv_1(input)
+        x = self.BN_1(x)
+        x = F.relu(x)
+        
+        x = self.Conv_2(x)
+        x = self.BN_2(x)
+        x = F.relu(x)
+        
+        x+=input
+        x = F.relu(x)
+
+        
+        x = self.Conv_3(x)
+        x = self.BN_3(x)
+        x = F.relu(x)
+        
+        return(x)
+
+      
+
+
+
 class Autoencoder(nn.Module):
     def __init__(self,
                  input_shape=(32,32,3)):
@@ -71,118 +104,36 @@ class Autoencoder(nn.Module):
        
         #visual parameters
         self.pool = nn.MaxPool2d(2, 2)
+        self.upSample= nn.Upsample(scale_factor=2, mode='bilinear')
         self.input_shape=input_shape
+        
+        N1=32
+        
+        Nblocks=64
         
         #first step
 
-        self.Conv0=nn.Conv2d(3,N1,3,stride=1,padding=1)
-        self.BN_1=torch.nn.BatchNorm2d(N1,affine=False)
-        self.Conv1=nn.Conv2d(N1,N1,3,stride=1,padding=1) 
-        self.BN_2=torch.nn.BatchNorm2d(N1,affine=False)
-
-        #block1
-        self.Conv_1B1=nn.Conv2d(N1,N1,3,stride=1,padding=1) 
-        self.BN_1B1=torch.nn.BatchNorm2d(N1,affine=False)
-        self.Conv_2B1=nn.Conv2d(N1,N1,1,stride=1,padding=0)
-        self.BN_2B1=torch.nn.BatchNorm2d(N1,affine=False)
-        self.Conv_3B1=nn.Conv2d(N1,N1,3,stride=1,padding=1) 
-        self.BN_3B1=torch.nn.BatchNorm2d(N1,affine=False)
-        self.Conv_4B1=nn.Conv2d(2*N1,N1,3,stride=1,padding=1) 
-        self.BN_4B1=torch.nn.BatchNorm2d(N1,affine=False)
-        self.Conv_5B1=nn.Conv2d(N1,N1,3,stride=1,padding=1) 
-        self.BN_5B1=torch.nn.BatchNorm2d(N1,affine=False)
-        self.Conv_6B1=nn.Conv2d(N1,N1,3,stride=1,padding=1) 
-        self.BN_6B1=torch.nn.BatchNorm2d(N1,affine=False)
-        self.Conv_7B1=nn.Conv2d(2*N1,N1,3,stride=1,padding=1) 
-        self.BN_7B1=torch.nn.BatchNorm2d(N1,affine=False)
-        self.Conv_8B1=nn.Conv2d(3,N1,3,stride=1,padding=1) 
-        self.BN_8B1=torch.nn.BatchNorm2d(N1,affine=False)
-        self.Conv_9B1=nn.Conv2d(2*N1,N1,3,stride=1,padding=1) 
-        self.BN_9B1=torch.nn.BatchNorm2d(N1,affine=False)
-
-
-        #block2
-        self.Conv_1B2=nn.Conv2d(N1,N2,3,stride=1,padding=1) 
-        self.BN_1B2=torch.nn.BatchNorm2d(N2,affine=False)
-        self.Conv_2B2=nn.Conv2d(N2,N2,1,stride=1,padding=0)
-        self.BN_2B2=torch.nn.BatchNorm2d(N2,affine=False)
-        self.Conv_3B2=nn.Conv2d(N2,N2,3,stride=1,padding=1) 
-        self.BN_3B2=torch.nn.BatchNorm2d(N2,affine=False)
-        self.Conv_4B2=nn.Conv2d(2*N2,N2,3,stride=1,padding=1) 
-        self.BN_4B2=torch.nn.BatchNorm2d(N2,affine=False)
-        self.Conv_5B2=nn.Conv2d(N2,N2,3,stride=1,padding=1) 
-        self.BN_5B2=torch.nn.BatchNorm2d(N2,affine=False)
-        self.Conv_6B2=nn.Conv2d(N1,N2,3,stride=1,padding=1) 
-        self.BN_6B2=torch.nn.BatchNorm2d(N2,affine=False)
-        self.Conv_7B2=nn.Conv2d(2*N2,N2,3,stride=1,padding=1) 
-        self.BN_7B2=torch.nn.BatchNorm2d(N2,affine=False)
-        self.Conv_8B2=nn.Conv2d(3,N2,3,stride=1,padding=1) 
-        self.Conv_9B2=nn.Conv2d(N2,N2,3,stride=1,padding=1) 
-        self.BN_9B2=torch.nn.BatchNorm2d(N2,affine=False)
-        self.Conv_10B2=nn.Conv2d(2*N2,N2,3,stride=1,padding=1) 
-        self.BN_10B2=torch.nn.BatchNorm2d(N2,affine=False)
-      
+        self.Conv_1P=nn.Conv2d(3,N1,3,stride=1,padding=1)
+        self.BN_1P=torch.nn.BatchNorm2d(N1)
+        self.Conv_2P=nn.Conv2d(N1,Nblocks,3,stride=1,padding=1) 
+        self.BN_2P=torch.nn.BatchNorm2d(Nblocks)
         
-        #upBlock1
-        self.Conv_1U1=nn.Conv2d(N2,N2,3,stride=1,padding=1) 
-        self.BN_1U1=torch.nn.BatchNorm2d(N2,affine=False)
-        self.Conv_2U1=nn.Conv2d(N2,N2,1,stride=1,padding=0)
-        self.BN_2U1=torch.nn.BatchNorm2d(N2,affine=False)
-        self.Conv_3U1=nn.Conv2d(N2,N2,3,stride=1,padding=1) 
-        self.BN_3U1=torch.nn.BatchNorm2d(N2,affine=False)
-        self.Conv_4U1=nn.Conv2d(2*N2,N2,3,stride=1,padding=1)
-        self.BN_4U1=torch.nn.BatchNorm2d(N2,affine=False)
-        self.UP_1U1=nn.ConvTranspose2d(N2, N2, 2, stride=2)
-        self.Conv_5U1=nn.Conv2d(N2,N2,3,stride=1,padding=1)
-        self.BN_5U1=torch.nn.BatchNorm2d(N2,affine=False)
-        self.UP_2U1=nn.ConvTranspose2d(N2, N2, 2, stride=2)
-        self.Conv_6U1=nn.Conv2d(N2,N2,3,stride=1,padding=1) 
-        self.BN_6U1=torch.nn.BatchNorm2d(N2,affine=False)
-        self.Conv_7U1=nn.Conv2d(2*N2,N2,3,stride=1,padding=1) 
-        self.BN_7U1=torch.nn.BatchNorm2d(N2,affine=False)
-        self.UP_3U1=nn.ConvTranspose2d(N2, N2, 2, stride=2)
-        self.Conv_8U1=nn.Conv2d(N2,N2,3,stride=1,padding=1) 
-        self.BN_8U1=torch.nn.BatchNorm2d(N2,affine=False)
-        self.Conv_9U1=nn.Conv2d(2*N2,N1,3,stride=1,padding=1) 
-        self.BN_9U1=torch.nn.BatchNorm2d(N1,affine=False)
+        #encoding blocks
+        self.Code_B1=Block(Nblocks)
+        self.Code_B2=Block(Nblocks)        
+        self.Code_B3=Block(Nblocks)
         
-        #upBlock2
-        self.Conv_1U2=nn.Conv2d(N1,N1,3,stride=1,padding=1) 
-        self.BN_1U2=torch.nn.BatchNorm2d(N1,affine=False)
-        self.Conv_2U2=nn.Conv2d(N1,N1,1,stride=1,padding=0) 
-        self.BN_2U2=torch.nn.BatchNorm2d(N1,affine=False)
-        self.Conv_3U2=nn.Conv2d(N1,N1,3,stride=1,padding=1) 
-        self.BN_3U2=torch.nn.BatchNorm2d(N1,affine=False)
-        self.Conv_4U2=nn.Conv2d(2*N1,N1,3,stride=1,padding=1)
-        self.BN_4U2=torch.nn.BatchNorm2d(N1,affine=False)
-        self.UP_1U2=nn.ConvTranspose2d(N1, N1, 2, stride=2)
-        self.Conv_5U2=nn.Conv2d(N1,N1,3,stride=1,padding=1)
-        self.BN_5U2=torch.nn.BatchNorm2d(N1,affine=False)
-        self.UP_2U2=nn.ConvTranspose2d(N1, N1, 2, stride=2)
-        self.Conv_6U2=nn.Conv2d(N1,N1,3,stride=1,padding=1) 
-        self.BN_6U2=torch.nn.BatchNorm2d(N1,affine=False)
-        self.Conv_7U2=nn.Conv2d(2*N1,N1,3,stride=1,padding=1) 
-        self.BN_7U2=torch.nn.BatchNorm2d(N1,affine=False)
-        self.UP_3U2=nn.ConvTranspose2d(N2, N2//2, 2, stride=2)
-        self.Conv_8U2=nn.Conv2d(N2//2,N2//2,3,stride=1,padding=1) 
-        #self.BN_8U2=torch.nn.BatchNorm2d(4,affine=False)
-        self.UP_4U2=nn.ConvTranspose2d(N2//4, N1, 2, stride=2)
-        self.Conv_9U2=nn.Conv2d(N1,N1,3,stride=1,padding=1) 
-        self.BN_9U2=torch.nn.BatchNorm2d(N1,affine=False)
-        self.Conv_10U2=nn.Conv2d(2*N1,N1,3,stride=1,padding=1) 
-        self.BN_10U2=torch.nn.BatchNorm2d(N1,affine=False)
+        #decoding blocks
+        self.DeCode_B1=Block(Nblocks)
+        self.DeCode_B2=Block(Nblocks)
+        self.DeCode_B3=Block(Nblocks)
+
         
         #last step
-        self.Conv2=nn.Conv2d(N1,N1,3,stride=1,padding=1) 
-        self.Conv3=nn.Conv2d(N1,3,3,stride=1,padding=1) 
-        self.Conv4=nn.Conv2d(3,3,3,stride=1,padding=1) 
-        self.Conv5=nn.Conv2d(3,3,3,stride=1,padding=1) 
-
-
-
-
-
-        
+        self.Conv_1F=nn.Conv2d(Nblocks,N1,3,stride=1,padding=1) 
+        self.Conv_2F=nn.Conv2d(N1,3,3,stride=1,padding=1) 
+        self.Conv_3F=nn.Conv2d(3,3,3,stride=1,padding=1) 
+        self.Conv_4F=nn.Conv2d(3,3,3,stride=1,padding=1)       
         
 
  
@@ -192,260 +143,54 @@ class Autoencoder(nn.Module):
     def forward(self,image):
         
 # =============================================================================
-#         CODE
+#         Pre Step
 # =============================================================================
         
-        #first step
-        x = self.Conv0(image)
-        x = self.BN_1(x)
+       
+        x = self.Conv_1P(image)
+        x = self.BN_1P(x)
         x = F.relu(x)
         
-        x = self.Conv1(x)
-        x = self.BN_2(x)
-        afterFirstStep = F.relu(x)
-
-
-
-        
-        #first block
-        x = self.Conv_1B1(afterFirstStep)
-        x = self.BN_1B1(x)
+        x = self.Conv_2P(x)
+        x = self.BN_2P(x)
         x = F.relu(x)
-        
-        y1 = self.Conv_2B1(x)
-        y1 = self.BN_2B1(y1)
-        y1 = F.relu(y1)
-        
-        y2 = self.Conv_3B1(x)
-        y2 = self.BN_3B1(y2)
-        y2 = F.relu(y2)
-        
-        x=torch.cat((y1,y2),1)
-        
-        x = self.Conv_4B1(x)
-        x = self.BN_4B1(x)
-         
-        x = F.relu(x)
+
+# =============================================================================
+#       CODE
+# =============================================================================
+        x=self.pool(x)   
+        x=self.Code_B1.forward(x)
         
         x=self.pool(x)
+        x=self.Code_B2.forward(x)
         
-        x = self.Conv_5B1(x)
-        x = self.BN_5B1(x)
-        z1 = F.relu(x)
-        
-        z2=self.pool(afterFirstStep)
-        z2=self.Conv_6B1(z2)
-        z2= self.BN_6B1(z2)
-        z2 = F.relu(z2)
-        
-        
-
-        x=torch.cat((z1,z2),1)
-
-        x = self.Conv_7B1(x)
-        x = self.BN_7B1(x)
-        q1 = F.relu(x)
-        
-        
-        q2=self.pool(image)
-        q2 = self.Conv_8B1(q2)
-        q2 = self.BN_8B1(q2)
-        q2 = F.relu(q2)
-        
-        x=torch.cat((q1,q2),1)
-
-        x = self.Conv_9B1(x)
-        x = self.BN_9B1(x)
-        afterFirstBlock = F.relu(x)
-        
-        
-        
-        #second block
-        x = self.Conv_1B2(afterFirstBlock)
-        x = self.BN_1B2(x)
-        x = F.relu(x)
-        
-        y1 = self.Conv_2B2(x)
-        y1 = self.BN_2B2(y1)
-        y1 = F.relu(y1)
-        
-        y2 = self.Conv_3B2(x)
-        y2 = self.BN_3B2(y2)
-        y2 = F.relu(y2)
-        
-        x=torch.cat((y1,y2),1)
-        
-        x = self.Conv_4B2(x)
-        x = self.BN_4B2(x)
-
-        x = F.relu(x)
-        
-        x=self.pool(x)        
-        x = self.Conv_5B2(x)
-        x = self.BN_5B2(x)
-
-        z1 = F.relu(x)
-        
-        z2=self.pool(afterFirstBlock)
-        z2 = self.Conv_6B2(z2)
-        z2 = self.BN_6B2(z2)
-
-        z2 = F.relu(z2)
-        
-        
-
-        x=torch.cat((z1,z2),1)
-
-        x = self.Conv_7B2(x)
-        x = self.BN_7B2(x)
-
-        q1 = F.relu(x)
-        
-        
-        q2=self.pool(image)
-        q2 = self.Conv_8B2(q2)
-        q2=self.pool(q2)
-        q2 = self.Conv_9B2(q2)
-        q2= self.BN_9B2(q2)
-
-        q2 = F.relu(q2)
-        
-        x=torch.cat((q1,q2),1)
-
-        x = self.Conv_10B2(x)
-        x = self.BN_10B2(x)
-        code = F.relu(x)
+        x=self.pool(x)
+        x=self.Code_B3.forward(x)
         
         
 # =============================================================================
 #         DECODE
 # =============================================================================
-               
-        #first UPblock
-        x = self.Conv_1U1(code)
-        x = self.BN_1U1(x)
-        x = F.relu(x)
+        x=self.upSample(x)   
+        x=self.DeCode_B1.forward(x)
         
-        y1 = self.Conv_2U1(x)
-        y1= self.BN_2U1(y1)
-        y1 = F.relu(y1)
+        x=self.upSample(x)
+        x=self.DeCode_B2.forward(x)
         
-        y2 = self.Conv_3U1(x)
-        y2= self.BN_3U1(y2)
-        y2 = F.relu(y2)
-        
-        x=torch.cat((y1,y2),1)
-        
-        x = self.Conv_4U1(x)
-        x= self.BN_4U1(x)
-
-        x = F.relu(x)
-        x=self.UP_1U1(x)
-        
-        x = self.Conv_5U1(x)
-        x= self.BN_5U1(x)
-
-        z1 = F.relu(x)
-        
-        z2=self.UP_2U1(code)
-        z2 = self.Conv_6U1(z2)
-        z2=self.BN_6U1(z2)
-        z2 = F.relu(z2)      
-        
-       
-
-        x=torch.cat((z1,z2),1)
-        
-
-        x = self.Conv_7U1(x)
-        x= self.BN_7U1(x)
-        q1 = F.relu(x)
-        
-        
-        q2=self.UP_3U1(code)
-        q2 = self.Conv_8U1(q2)
-        q2= self.BN_8U1(q2)
-
-        q2 = F.relu(q2)
-        
-        x=torch.cat((q1,q2),1)
-
-        x = self.Conv_9U1(x)
-        x= self.BN_9U1(x)
-
-        afterFirstUp = F.relu(x)
-        
-        
-        
-        
-        
-        
-        #   second UPblock
-        x = self.Conv_1U2(afterFirstUp)
-        x= self.BN_1U2(x)
-        x = F.relu(x)
-        
-        y1 = self.Conv_2U2(x)
-        y1= self.BN_2U2(y1)
-        y1 = F.relu(y1)
-        
-        y2 = self.Conv_3U2(x)
-        y2= self.BN_3U2(y2)
-        y2 = F.relu(y2)
-        
-        x=torch.cat((y1,y2),1)
-        
-        x = self.Conv_4U2(x)
-        x= self.BN_4U2(x)
-        x = F.relu(x)
-        
-        
-        x=self.UP_1U2(x)
-        
-        x = self.Conv_5U2(x)
-        x= self.BN_5U2(x)
-        z1 = F.relu(x)
-        
-        z2=self.UP_2U2(afterFirstUp)
-        z2 = self.Conv_6U2(z2)
-        z2= self.BN_6U2(z2)
-        z2 = F.relu(z2)
-        
-        
-
-        x=torch.cat((z1,z2),1)
-        x = self.Conv_7U2(x)
-        x= self.BN_7U2(x)
-        q1 = F.relu(x)
-        
-        
-        q2=self.UP_3U2(code)
-        q2 = self.Conv_8U2(q2)
-        q2=self.UP_4U2(q2)
-        q2 = self.Conv_9U2(q2)
-        q2= self.BN_9U2(q2)
-        q2 = F.relu(q2)
-        
-        x=torch.cat((q1,q2),1)
-
-        x = self.Conv_10U2(x)
-        x= self.BN_10U2(x)
-        afterSecondUp = F.relu(x)
-        
-        
- 
+        x=self.upSample(x)
+        x=self.DeCode_B3.forward(x)
         
 # =============================================================================
 #         Last step
 # =============================================================================
         
-        x=self.Conv2(afterSecondUp)
+        x=self.Conv_1F(x)
         x = F.relu(x)
-        x=self.Conv3(x)
+        x=self.Conv_2F(x)
         x = F.relu(x)
-        x=self.Conv4(x)
+        x=self.Conv_3F(x)
         x = F.relu(x)
-        x=self.Conv5(x)
+        x=self.Conv_4F(x)
         output = F.relu(x)       
         
         
