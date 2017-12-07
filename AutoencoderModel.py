9de055ecc1cca71ce22f078e020ac836e70f96d4
simon555 <simon.sebbagh@free.fr>
2017-12-07 13:30:34 -0500

    training on MNIST

--- AutoencoderModel.py
+++ AutoencoderModel.py
@@ -41,12 +41,12 @@ transform = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+trainset = torchvision.datasets.MNIST(root='./MNIST', train=True,
                                         download=True, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=NbatchTrain,
                                           shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+testset = torchvision.datasets.MNIST(root='./MNIST', train=False,
                                        download=True, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=NbatchTest,
                                          shuffle=False, num_workers=2)
